fdaff3a8ef03d2386796a85b40228d0a6c0a7694
Anthony Lenglet <anthony.lenglet@hotmail.com>
2026-07-23 11:00:39 +0200

    fix: various swiftformat errors

--- Sources/Kaset/Views/ArtistDetailView.swift
+++ Sources/Kaset/Views/ArtistDetailView.swift
@@ -819,8 +819,8 @@ struct ArtistDetailView: View { // swiftlint:disable:this type_body_length
                 PlayableArtistCard(playAction: self.quickPlayAction(for: playlist), thumbnailSize: 140) {
                     VStack(alignment: .leading, spacing: 8) {
                         CachedAsyncImage(
-                          url: playlist.thumbnailURL?.highQualityThumbnailURL,
-                          targetSize: CGSize(width: 140, height: 140)
+                            url: playlist.thumbnailURL?.highQualityThumbnailURL,
+                            targetSize: CGSize(width: 140, height: 140)
                         ) { image in image
                             .resizable()
                             .aspectRatio(contentMode: .fill)
